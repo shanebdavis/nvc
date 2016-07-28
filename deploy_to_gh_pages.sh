c551296ffcc0e1f1950cb98a45e8451d7f446c75
Shane Brinkman-Davis <shanebdavis@gmail.com>
2016-07-28 10:26:03 -0600

    deploy more specific

--- deploy_to_gh_pages.sh
+++ deploy_to_gh_pages.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cp cordova/www/app.js gh_pages_transfer/
 if git checkout gh-pages; then
-  cp gh_pages_transfer/* .
-  git add *
+  cp gh_pages_transfer/app.js .
+  git add app.js
   git commit -m "deploy"
   git push
   git checkout master
