49e6859c7cd56d5fa4f5a6dde96c5335f98c4dda
Shane Brinkman-Davis <shanebdavis@gmail.com>
2016-07-28 10:22:49 -0600

    deploy script should block if there are local changes

--- deploy_to_gh_pages.sh
+++ deploy_to_gh_pages.sh
@@ -1,9 +1,12 @@
-
+#!/bin/bash
 cp cordova/www/app.js gh_pages_transfer/
-git checkout gh-pages
-cp gh_pages_transfer/* .
-git add *
-git commit -m "deploy"
-git push
-git checkout master
-open https://shanebdavis.github.io/nvc/
+if git checkout gh-pages; then
+  cp gh_pages_transfer/* .
+  git add *
+  git commit -m "deploy"
+  git push
+  git checkout master
+  open https://shanebdavis.github.io/nvc/
+else
+  echo "Couldn't switch branches. Make sure your branch is clean."
+fi
