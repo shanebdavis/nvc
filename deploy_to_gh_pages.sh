cd4d897c55e7808f7e075d25d062238cf1a2fb97
Shane Brinkman-Davis <shanebdavis@gmail.com>
2016-10-20 11:42:02 -0600

    webpack added

--- deploy_to_gh_pages.sh
+++ deploy_to_gh_pages.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+webpack
+git commit cordova/www/app.js -m "updated cordova/www/app.js"
 cp cordova/www/app.js gh_pages_transfer/
 if git checkout gh-pages; then
   cp gh_pages_transfer/app.js .
