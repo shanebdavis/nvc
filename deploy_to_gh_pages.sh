df5800d0e54542a38dace6314b0a89889efc7aec
Shane Brinkman-Davis <shanebdavis@gmail.com>
2016-10-25 10:18:02 -0600

    nicer output

--- deploy_to_gh_pages.sh
+++ deploy_to_gh_pages.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
-echo "\nWebpack-Building app.js..."
-webpack
+echo ">>> Webpack-Building app.js..."
+time webpack
 git commit cordova/www/app.js -m "updated cordova/www/app.js"
 cp cordova/www/app.js gh_pages_transfer/
 cp -R assets gh_pages_transfer/
-echo "\nChecking out gh-pages..."
+echo ">>> Checking out gh-pages..."
 if git checkout gh-pages; then
 
-  echo "\nCopying assets..."
+  echo ">>> Copying assets..."
   cp -R gh_pages_transfer/assets .
 
-  echo "\nUglifying (-c)..."
-  uglifyjs gh_pages_transfer/app.js -c > ./app.js
+  echo ">>> Uglifying (-c)..."
+  time uglifyjs gh_pages_transfer/app.js -c > ./app.js
 
-  echo "\nCommitting and pushing updates..."
+  echo ">>> Committing and pushing updates..."
   git add app.js assets
   git commit -m "deploy"
   git push
-  echo "\nSwitching back to out master..."
+  echo ">>> Switching back to out master..."
   git checkout master
   open https://shanebdavis.github.io/nvc/
-  echo "\ndone."
+  echo ">>> done."
 else
   echo "Couldn't switch branches. Make sure your branch is clean."
 fi
